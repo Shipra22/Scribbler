dd666e9e79b00b40b81181effb7ffb3f7d401c0b
Shipra22 <shiprarai.lko@gmail.com>
2020-12-06 20:57:10 +0530

    Home Page Created

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+
+function goToPostList() {
+    location.href = "/home/shipra/Downloads/upgrad/Project Scribbler/Practice Scribbler/html/postslist.html"
+}
+
+function showCreatePostModal() {
+    // document.getElementById("create-post-modal").style.display = "block";
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "flex";
+}
+function closeCreatePostModal() {
+    var modal = document.getElementById("create-post-modal");
+    modal.style.display = "none";
+}
